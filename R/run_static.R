929c120794678a42ad30ff8867029e8cb97eb90f
Clark Rushing <crushing05@gmail.com>
2017-08-17 11:27:00 -0400

    Multiple covariates w/ linear + quadratic effects; GVS

--- R/run_static.R
+++ R/run_static.R
@@ -12,16 +12,20 @@
 
 run_static <- function(sim_data, jagam.mod, nC = 3, nI = 25000, nB = 5000, nT = 10, Parallel = TRUE){
 
+    mu.est <- sim_data$beta
+    se.est <- rep(0.25, dim(sim_data$X)[2])
+
     jags.data <- list(h = sim_data$h, nStops = sim_data$nStops, nRoutes = sim_data$nRoutes,
                       stop = sim_data$stop, stop2 = sim_data$stop2, Xp = sim_data$Xp,
-                      X1 = sim_data$X1, nPred = dim(sim_data$X1)[2],
-                      X = jagam.mod$jags.data$X, S1 = jagam.mod$jags.data$S1, zero = jagam.mod$jags.data$zero)
+                      X1 = sim_data$X, nPred = dim(sim_data$X)[2]/2,
+                      X = jagam.mod$jags.data$X, S1 = jagam.mod$jags.data$S1, zero = jagam.mod$jags.data$zero,
+                      mu = mu.est, se = se.est)
 
-    jags.params <- c("xpsi", "lambda", "beta", "g", "alpha0", "alpha1", "psi", "p", "z")
+    jags.params <- c("xpsi", "lambda", "beta", "g", "alpha0", "alpha1", "pind", "psi", "p", "z")
 
     jags.inits <- function(){list(y = sim_data$y, z = apply(sim_data$y, 1, max),
                                   alpha0 = rnorm(1), alpha1 = rnorm(1), alpha2 = rnorm(1),
-                                  betaT = rnorm(dim(sim_data$X1)[2]), sigma.beta = runif(1, 0, 5),
+                                  betaT = rnorm(dim(sim_data$X)[2]), sigma.beta = runif(1, 0, 5),
                                   pind = runif(1, 0, 1),
                                   xpsi = runif(2, 0, 1), b = jagam.mod$jags.ini$b,
                                   lambda = jagam.mod$jags.ini$lambda)}
--- R/sim_data_static.R
+++ R/sim_data_static.R
@@ -6,19 +6,15 @@
 #' @param alpha0 Intercept for detection model
 #' @param alpha1 Linear effect of Xp on detection probability
 #' @param sigma1 SD of Xp
-#' @param beta0 Logit expected occupancy probability
-#' @param beta1 Linear effect of X on psi
-#' @param beta2 Quadratic effect of X on psi
 #' @param sigma2 SD of X
 #' @param sigma3 Noise
 #' @param theta Vector containing the correlation terms
 #' @param nSum Optional number of stops to aggregate
 #' @export
 
-sim_data_static <- function(nRoutes = 150, nStops = 50,
+sim_data_static <- function(nRoutes = 150, nStops = 50, nPred = 5,
                              alpha0 = -0.5, alpha1 = 0.75, sigma1 = 0.5,
-                             beta0 = -2, beta1 = 0.8, beta2 = -1,
-                             sigma2 = 0.25, sigma3 = 0.05,
+                             sigma2 = 0.5, sigma3 = 0.05,
                              theta = c(0.3, 0.75), sx = 0.2, sy = 0.2,
                              nSum = NULL){
 
@@ -30,24 +26,36 @@ sim_data_static <- function(nRoutes = 150, nStops = 50,
                     y = (xy$y - min(xy$y))/(max(xy$y) - min(xy$y)))
   sigma <- cov(sxy)
 
+
   ## Standardize stop number
   stop <- scale(seq(1:nStops))[,1]
   stop2 <- stop^2
 
-  ## Stop detection probability
-  Xp <- rnorm(nRoutes, sigma1)
-  lp <- alpha0 + alpha1 * Xp#stop + alpha2 * stop2
+
+  ## Stop-level detection probability w/ 1 covariate
+  Xp <- rnorm(nRoutes, sd = sigma1)
+  lp <- alpha0 + alpha1 * Xp
   p <- exp(lp)/(1 + exp(lp))
 
-  ## Route-level occupancy probability
-  X1 <- rnorm(nRoutes, sigma2)
+
+  ## Route-level occupancy probability w/ lat/lon spline & nPred covariates (linear + quadratic effects)
+  beta1 <- rgamma(nPred, 1, 1)    # Linear slopes
+  beta2 <- -rgamma(nPred, 1, 1)   # Quadratic slopes
+  beta <- c(beta1, beta2)
+
+  X1 <- matrix(rnorm(nRoutes * nPred, sd = sigma2), nrow = nRoutes, ncol = nPred) # Covariate values
   X2 <- X1^2
-  eta <- rnorm(nRoutes, 0, sigma3)
-  f <- beta0 + (1/(2*pi*sx*sy*sqrt(1 - sigma[1,2]))) *
+  X <- cbind(X1, X2)
+
+  eta <- rnorm(nRoutes, sd = sigma3)   # Noise
+
+  f <- (1/(2*pi*sx*sy*sqrt(1 - sigma[1,2]))) *
         exp(-(1/(2*(1-sigma[1,2]^2))*
         (sxy$x-0.5)^2/sx^2 + (sxy$y - 0.5)^2/sy^2 - (2*sigma[1,2]*(sxy$x - 0.5)*(sxy$y - 0.5))/sx*sy))
-  l.psi <- f + beta1 * X1 + beta2 * X2 + eta
+
+  l.psi <- f + X %*% beta + eta
   psi <- exp(l.psi)/(1 + exp(l.psi))
+
 
   # Route-level occupancy
   z <- rbinom(n = nRoutes, size = 1, prob = psi)
@@ -57,8 +65,7 @@ sim_data_static <- function(nRoutes = 150, nStops = 50,
   theta1 <- theta[1] / (theta[1] + (1 - theta[2]))
 
   ## Stop level data
-  y <- matrix(NA, nrow = nRoutes, ncol = nStops)  # Stop availability
-  h <- matrix(NA, nrow = nRoutes, ncol = nStops)  # Observed presence/absence
+  y <- h <- matrix(NA, nrow = nRoutes, ncol = nStops)  # Stop availability & observed occupancy
 
   for(i in 1:nRoutes){
     y[i, 1] <- rbinom(n = 1, size = 1, prob = z[i] * theta1)
@@ -97,11 +104,9 @@ sim_data_static <- function(nRoutes = 150, nStops = 50,
     h <- h2
   }
 
-  ## Combine covariate values in matrix
-  X1 <- cbind(X1, X2)
 
-  sim_data <- list(psi = psi, z = z, h = h, y = y, p = p, Xp = Xp, X1 = X1, xy = xy, nRoutes = nRoutes, nStops = nStops,
-                   beta0 = beta0, sigma1 = sigma1, sigma2 = sigma2, sigma3 = sigma3, alpha0 = alpha0, alpha1 = alpha1, #alpha2 = alpha2,
+  sim_data <- list(psi = psi, z = z, h = h, y = y, p = p, Xp = Xp, X = X, xy = xy, nRoutes = nRoutes, nStops = nStops,
+                   beta0 = beta0, beta = beta, sigma1 = sigma1, sigma2 = sigma2, sigma3 = sigma3, alpha0 = alpha0, alpha1 = alpha1, #alpha2 = alpha2,
                    stop = stop, stop2 = stop2, theta = theta)
 
   return(sim_data)
